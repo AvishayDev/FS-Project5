32b413cefcb4549c8f0695ca2e3072c15d0681c2
sam7-5 <versesamuel@gmail.com>
2023-05-15 00:12:57 +0300

    info

--- src/Info.js
+++ src/Info.js
@@ -1,7 +1,45 @@
-import { useOutletContext } from "react-router-dom";
+import React, { useState, useEffect } from 'react';
+import { useParams } from "react-router-dom";
 
-export default function Info(){
-    const context = useOutletContext()
+const InfoComponent = () => {
+  const [user, setUser] = useState({});
+  const { id } = useParams()
+  console.log(id)
+  
+  useEffect(() => {
+    const fetchUser = async () => {
+      try {
+        // Fetch user data from api
+        const response = await fetch(
+          `https://jsonplaceholder.typicode.com/users/${id}`
+        );
+        const data = await response.json();
+        console.log(data);
+        setUser(data);
+        console.log(user);
+      } catch (error) {
+        console.log(error);
+      }
+    };
 
-    return (<h1>Info</h1>)
-}
+    fetchUser();
+  }, [id]);
+
+  return (
+    <div>
+      {user.name ? (
+        <div>
+          <h2>{user.name}</h2>
+          <p>Username: {user.username}</p>
+          <p>Email: {user.email}</p>
+          <p>Phone: {user.phone}</p>
+          {/* Render additional user information as needed */}
+        </div>
+      ) : (
+        <p>Loading user information...</p>
+      )}
+    </div>
+  );
+};
+
+export default InfoComponent;
